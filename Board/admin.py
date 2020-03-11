68d725be01b1040b9c878818f6d368921173cd29
SeongMin Han <hsm950825@naver.com>
2020-03-11 22:49:34 +0900

    자유게시판 / 댓글 / 좋아요 admin 생성

--- Board/admin.py
+++ Board/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Board, Comments, Likes
 
-# Register your models here.
+
+class BoardAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'author', 'title', 'likes_count')
+
+
+admin.site.register(Board, BoardAdmin)
+
+
+class CommentsAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'creator', 'message', 'created_at', 'updated_at')
+
+
+admin.site.register(Comments, CommentsAdmin)
+
+
+class LikesAdmin(admin.ModelAdmin):
+
+    list_display = ('id', 'creator', 'post')
+
+
+admin.site.register(Likes, LikesAdmin)
