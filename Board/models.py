2142a952b6cf869051ac26832c317772eac99a70
SeongMin Han <hsm950825@naver.com>
2020-03-11 22:49:25 +0900

    자유게시판 / 댓글 / 좋아요 모델 생성

--- Board/models.py
+++ Board/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Board(TimeStampedModel):
+    """ 자유게시판 (꿀팁/사기관련 정보) 테이블
+
+    * To do
+    1. author -> User 외래키로 변경
+    """
+    author = models.CharField(max_length=20, blank=True)
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+
+    # 좋아요 수
+    @property
+    def likes_count(self):
+        return self.likes.all().count()
+
+    # 댓글 수
+    @property
+    def comments_count(self):
+        return self.comments.all().count()
+
+    def __str__(self):
+        return self.title
+
+
+class Comments(TimeStampedModel):
+    """ 게시판 관련 댓글 테이블 
+
+    * To do
+    1. creator -> User 외래키로 변경
+    """
+    creator = models.CharField(max_length=20, null=True)
+    message = models.TextField()
+    post = models.ForeignKey(
+        Board, related_name='comments', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.creator, self.message)
+
+
+class Likes(TimeStampedModel):
+    """ 좋아요 테이블
+
+    * To do
+    1. creator -> User 외래키로 변경
+    """
+    creator = models.CharField(max_length=20, null=True)
+    post = models.ForeignKey(
+        Board, related_name='likes', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return '{} - {}'.format(self.creator, self.post)
