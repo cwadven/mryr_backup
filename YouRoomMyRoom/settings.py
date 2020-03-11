498b31488869eb0d3c921fb678005da6d58fbd33
SeongMin Han <hsm950825@naver.com>
2020-03-11 12:16:21 +0900

    Django REST framework 설정

--- YouRoomMyRoom/settings.py
+++ YouRoomMyRoom/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Django REST framework
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -80,6 +83,13 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
