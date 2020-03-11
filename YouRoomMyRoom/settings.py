a2bee87746c6331783ac3ae1260b8aea6c4ac9ae
SeongMin Han <hsm950825@naver.com>
2020-03-11 12:22:00 +0900

    CORS 설정

--- YouRoomMyRoom/settings.py
+++ YouRoomMyRoom/settings.py
@@ -40,11 +40,14 @@ INSTALLED_APPS = [
 
     # Django REST framework
     'rest_framework',
+    # CORS
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -128,3 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_ALLOW_ALL = False
+
+CORS_ORIGIN_WHITELIST = [
+    "http://localhost:3000",
+    "http://127.0.0.1:8000"
+]
