12911a69cf601fbfa6f53e93146ccd5af401b2cc
cwadven <cwadven@naver.com>
2020-03-17 21:36:41 +0900

    좋아요 중복 없애기

--- Board/urls.py
+++ Board/urls.py
@@ -7,7 +7,7 @@ from rest_framework.routers import DefaultRouter # class 를 경로 지정하기
 from . import views
 
 router = DefaultRouter() #view에 있는 함수(클래스를 가져오기 위해서는 router 사용)
-router.register('', views.BoardViewset)
+router.register('Board', views.BoardViewset)
 router.register('Comments', views.Board_CommentsViewset)
 router.register('Likes', views.Board_LikessViewset)
 
--- Board/views.py
+++ Board/views.py
@@ -4,6 +4,9 @@ from .serializer import *
 from rest_framework import viewsets
 from rest_framework import permissions
 from .permissions import IsOwnerOrReadOnly
+from rest_framework.response import Response
+from rest_framework import status
+
 # Create your views here.
 
 class BoardViewset(viewsets.ModelViewSet):
@@ -38,8 +41,21 @@ class Board_LikessViewset(viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticatedOrReadOnly,
                       IsOwnerOrReadOnly,) #로그인된 사람은 쓸 수 있고 대신, 자기만의 것은 자기만 수정 가능  --> permissions.py 에서 class 만들어서 적용
 
+
     def perform_create(self, serializer): #자동으로 자기 자신 author에 저장 되도록
-        serializer.save(author=self.request.user)
+        qs = super().get_queryset()
+        qs = qs.filter(author=self.request.user, post=serializer.validated_data["post"]) #해당 게시판에 있을 경우 serializer.validated_data를 통해서 가져온 속성을 체크할 수 있다
+        if qs:
+            #print("있어!!") #추가적인 like를 못하게 설정
+            return Response(status=status.HTTP_404_NOT_FOUND) #추후에 에러 추가 필요 !!!!!!!!!!!!!!!
+        else:
+            serializer.save(author=self.request.user)
 
     def perform_update(self, serializer): #자동으로 자기 자신 author에 저장 되도록
-        serializer.save(author=self.request.user)
+        qs = super().get_queryset()
+        qs = qs.filter(author=self.request.user, post=serializer.validated_data["post"]) #해당 게시판에 있을 경우 serializer.validated_data를 통해서 가져온 속성을 체크할 수 있다
+        if qs:
+            #print("있어!!") #추가적인 like를 못하게 설정
+            return Response(status=status.HTTP_404_NOT_FOUND) #추후에 에러 추가 필요 !!!!!!!!!!!!!!!
+        else:
+            serializer.save(author=self.request.user)
